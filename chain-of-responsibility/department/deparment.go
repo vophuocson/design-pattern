66a9ba6cc7aacf74c35997e9662c3cbbe0ecf813
son-vp1998 <son-vp@genkisystem.com>
2025-03-21 21:45:02 +0700

    add chain of responsibility pattern

---
+++ chain-of-responsibility/department/deparment.go
@@ -0,0 +1,83 @@
+package department
+
+import (
+	"design-pattern-practice/chain-of-responsibility/patient"
+	"fmt"
+)
+
+type Department interface {
+	Execute(*patient.Patient)
+	SetNext(Department)
+}
+
+type Reception struct {
+	next Department
+}
+
+func (d *Reception) Execute(p *patient.Patient) {
+	if p.RegistrationDone {
+		fmt.Println("Patient registration already done")
+		d.next.Execute(p)
+		return
+	}
+	p.RegistrationDone = true
+	d.next.Execute(p)
+}
+
+func (d *Reception) SetNext(department Department) {
+	d.next = department
+}
+
+type Doctor struct {
+	next Department
+}
+
+func (d *Doctor) Execute(p *patient.Patient) {
+	if p.DoctorCheckUpDone {
+		fmt.Println("Doctor checkup already done")
+		d.next.Execute(p)
+		return
+	}
+	p.DoctorCheckUpDone = true
+	d.next.Execute(p)
+}
+
+func (d *Doctor) SetNext(department Department) {
+	d.next = department
+}
+
+type Medical struct {
+	next Department
+}
+
+func (d *Medical) Execute(p *patient.Patient) {
+	if p.MedicineDone {
+		fmt.Println("Medical checkup already done")
+		d.next.Execute(p)
+		return
+	}
+	p.MedicineDone = true
+	d.next.Execute(p)
+}
+
+func (d *Medical) SetNext(department Department) {
+	d.next = department
+}
+
+type Cashier struct {
+	next Department
+}
+
+func (d *Cashier) Execute(p *patient.Patient) {
+	if p.PaymentDone {
+		fmt.Println("Cashier checkup already done")
+		d.next.Execute(p)
+		return
+	}
+	p.PaymentDone = true
+	d.next.Execute(p)
+}
+
+func (d *Cashier) SetNext(department Department) {
+	d.next = department
+}
---
+++ chain-of-responsibility/process-factory/factory.go
@@ -0,0 +1,34 @@
+package processfactory
+
+import (
+	"design-pattern-practice/chain-of-responsibility/department"
+	"design-pattern-practice/chain-of-responsibility/patient"
+)
+
+type Process interface {
+	ReceivePatient(p *patient.Patient)
+}
+
+type NormalCare struct {
+	D department.Department
+}
+
+func (f *NormalCare) ReceivePatient(p *patient.Patient) {
+	f.D.Execute(p)
+}
+
+func FactoryProcess(processName string) Process {
+	if processName == "normal" {
+		var d department.Department
+		cashier := department.Cashier{}
+		medical := department.Medical{}
+		medical.SetNext(&cashier)
+		doctor := department.Doctor{}
+		doctor.SetNext(&medical)
+		reception := department.Reception{}
+		reception.SetNext(&doctor)
+		d = &reception
+		return &NormalCare{D: d}
+	}
+	return nil
+}
