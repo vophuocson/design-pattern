891baed55823130a22585e098ae33d1057bf3a8d
son-vp1998 <son-vp@genkisystem.com>
2025-04-05 09:44:23 +0700

    comment code

--- structural-pattern/proxy/client.go
+++ structural-pattern/proxy/client.go
@@ -4,6 +4,8 @@ import "design-pattern-practice/structural-pattern/proxy/graphic"
 
 func main() {
 	var g graphic.Graphic
+	// Initialize and hold a placeholder for the image
 	g = graphic.NewProxyImage("./image1/png")
+	// Suppose the image is only drawn when the user clicks to load it.
 	g.Draw()
 }
